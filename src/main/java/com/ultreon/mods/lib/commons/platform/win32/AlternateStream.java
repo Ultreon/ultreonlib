bdcd3d4780620a42dc3dc6d1b02080eb4909d8d3
Qboi123 <qboiwastaken@gmail.com>
2022-10-02 22:19:44 +0200

    Added some more utils.

---
+++ src/main/java/com/ultreon/mods/lib/commons/platform/win32/AlternateStream.java
@@ -0,0 +1,28 @@
+package com.ultreon.mods.lib.commons.platform.win32;
+
+import java.io.*;
+
+/**
+ * Alternate Data Streams
+ */
+public record AlternateStream(File file, String id) {
+    public OutputStream openOutputStream() throws FileNotFoundException {
+        return new FileOutputStream(file.getPath() + ":" + id);
+    }
+
+    public InputStream openInputStream() throws FileNotFoundException {
+        return new FileInputStream(file.getPath() + ":" + id);
+    }
+
+    public Reader openReader() throws FileNotFoundException {
+        return new FileReader(file.getPath() + ":" + id);
+    }
+
+    public Writer openWriter() throws IOException {
+        return new FileWriter(file.getPath() + ":" + id);
+    }
+
+    public String getPath() {
+        return file.getPath() + ":" + id;
+    }
+}
