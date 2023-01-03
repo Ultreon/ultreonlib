100ade88cd9c0e22887cfdb034af1349ab233348
Qboi123 <qboiwastaken@gmail.com>
2023-01-03 14:56:30 +0100

    Uh

--- common/src/main/java/com/ultreon/commons/lang/holders/BaseHolder.java
+++ common/src/main/java/com/ultreon/commons/lang/holders/BaseHolder.java
@@ -5,7 +5,6 @@ import net.minecraft.resources.ResourceLocation;
 
 public interface BaseHolder extends ComponentHolder, TranslationHolder, RegistryEntryHolder {
     @Override
-
     ResourceLocation getRegistryName();
 
     default String getName() {
