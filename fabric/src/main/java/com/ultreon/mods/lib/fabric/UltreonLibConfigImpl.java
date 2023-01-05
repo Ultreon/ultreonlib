f2c8045a08706e8bdd540734dae1003d40cdbe6b
Qboi123 <qboiwastaken@gmail.com>
2023-01-05 20:50:53 +0100

    Fix deprecated warning. (1.3.0)

--- fabric/src/main/java/com/ultreon/mods/lib/fabric/UltreonLibConfigImpl.java
+++ fabric/src/main/java/com/ultreon/mods/lib/fabric/UltreonLibConfigImpl.java
@@ -2,11 +2,11 @@ package com.ultreon.mods.lib.fabric;
 
 import com.ultreon.mods.lib.UltreonLib;
 import com.ultreon.mods.lib.UltreonLibConfig;
-import net.minecraftforge.api.ModLoadingContext;
+import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
 import net.minecraftforge.fml.config.ModConfig;
 
 public class UltreonLibConfigImpl {
     public static void register(Object ignored) {
-        ModLoadingContext.registerConfig(UltreonLib.MOD_ID, ModConfig.Type.CLIENT, UltreonLibConfig.CLIENT);
+        ForgeConfigRegistry.INSTANCE.register(UltreonLib.MOD_ID, ModConfig.Type.CLIENT, UltreonLibConfig.CLIENT);
     }
 }
