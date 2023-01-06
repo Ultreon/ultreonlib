543da551df06f4d4a34f7ca86309a189b6d002c5
Qboi123 <qboiwastaken@gmail.com>
2023-01-06 11:03:32 +0100

    Fix fabric dev preview detection

--- fabric/src/main/java/com/ultreon/mods/lib/fabric/UltreonLibFabricClient.java
+++ fabric/src/main/java/com/ultreon/mods/lib/fabric/UltreonLibFabricClient.java
@@ -1,10 +1,6 @@
 package com.ultreon.mods.lib.fabric;
 
-import com.ultreon.mods.lib.DevPreviewRegistry;
-import com.ultreon.mods.lib.UltreonLib;
 import com.ultreon.mods.lib.client.UltreonLibClient;
-import dev.architectury.platform.Mod;
-import dev.architectury.platform.Platform;
 import net.fabricmc.api.ClientModInitializer;
 
 public class UltreonLibFabricClient implements ClientModInitializer {
@@ -13,10 +9,6 @@ public class UltreonLibFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ultreonLib = UltreonLibClient.create();
-
-        for (Mod mod : Platform.getMods()) {
-            DevPreviewRegistry.register(mod);
-        }
     }
 
     public UltreonLibClient getUltreonLib() {
